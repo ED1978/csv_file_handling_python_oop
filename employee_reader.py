57d5c6ef4fe383055f93b422863ef04170b233ee
EDownie <53910027+ED1978@users.noreply.github.com>
2020-01-21 16:42:04 +0000

    can calculate amount due and enter into csv file for each employee

--- employee_reader.py
+++ employee_reader.py
@@ -13,9 +13,17 @@ with open("employees.csv", "r") as csvfile:
         current_employee = Employee(*row)
         all_employees.append(current_employee)
 
-with open("employees.csv", "a") as csvfile:
+# with open("employees.csv", "a") as csvfile:
+#     writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+#
+#     new_employee = ("Jenny", "Jones", 12.50, 40, 0)
+#
+#     writer.writerow(new_employee)
+
+with open("employees.csv", "w") as csvfile:
     writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+    writer.writerow(["first_name", "last_name", "hourly_rate", "hours_worked", "amount_due"])
 
-    new_employee = ("Jenny", "Jones", 12.50, 40, 0)
-
-    writer.writerow(new_employee)
+    for employee in all_employees:
+        employee.amount_due = float(employee.hours_worked) * float(employee.hourly_rate)
+        writer.writerow(employee.values())
